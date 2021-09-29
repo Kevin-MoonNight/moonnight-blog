1d563dde2142bc12182105f8eceffe489cfd22c7
MoonNight <k0937477331@gmail.com>
2021-09-29 15:45:57 +0800

    add footer data 'name'

--- resources/js/components/frontend/FooterComponent.vue
+++ resources/js/components/frontend/FooterComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="h-56 bg-gray-800 text-gray-200 py-2 text-md">
-        <span class="flex items-end justify-center h-full">Copyright &copy; {{ year }} by MoonNight All Rights Reserved.</span>
+        <span class="flex items-end justify-center h-full">Copyright &copy; {{ year }} by {{ name }} All Rights Reserved.</span>
     </div>
 </template>
 
@@ -8,6 +8,7 @@
     export default {
         data (){
             return {
+                name:'鄭政文',
                 year: new Date().getFullYear()
             }
         }
